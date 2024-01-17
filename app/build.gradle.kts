26922ab859becf350ee2f0ae5aa0d1f42372441f
Badizawr <vicodin-13@ya.ru>
2024-01-18 00:15:55 +0500

    ZXing Android Embedded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
--- app/src/main/java/com/example/qrcodescanerjetpack/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanerjetpack/MainActivity.kt
@@ -1,46 +1,57 @@
 package com.example.qrcodescanerjetpack
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.qrcodescanerjetpack.ui.theme.QRcodeScanerJetpackTheme
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : ComponentActivity() {
+    private val scanLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result ->
+        if (result.contents == null) {
+
+        } else {
+            Toast.makeText(
+                this, "Scan data: ${result.contents}", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QRcodeScanerJetpackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(
+                    modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        scan()
+                    }) {
+                        Text(text = "Scan")
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun scan() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.ALL_CODE_TYPES)
+        options.setPrompt("Point the camera at barcode or QR")
+        options.setCameraId(0)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QRcodeScanerJetpackTheme {
-        Greeting("Android")
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        scanLauncher.launch(options)
     }
 }
